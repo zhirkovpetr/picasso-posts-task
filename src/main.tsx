647890b34d00c44f90c345856ad8501266ec07c8
Petr <zhirkovpetr95@gmail.com>
2024-04-18 16:21:15 +0300

    refactor: refactor code

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import {App, store} from "./app";
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename="/picasso-posts-task">
       <App/>
     </BrowserRouter>
   </Provider>
